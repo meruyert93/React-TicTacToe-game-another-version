d8cd48a1ed4874127d62c5fc6518511fee360697
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-02-06 20:34:19 +0100

    onSquareClick functionality

--- src/App.js
+++ src/App.js
@@ -7,25 +7,28 @@ const initialState = ["", "", "", "", "", "","", "", ""];
 function App() {
     const [gameState, setGameState] = useState(initialState);
 
+    const onSquareClick = (index) => {
+        
+    }
 
 
     return (
         <div className="app-header">
             <p className="heading-text">React Tic Tac Toe - 2021</p>
             <div className="row jc-center">
-                <SquareComponent className="b-bottom-right" state={gameState[0]}/>
-                <SquareComponent className="b-bottom-right" state={gameState[1]}/>
-                <SquareComponent className="b-bottom" state={gameState[2]}/>
+                <SquareComponent className="b-bottom-right" state={gameState[0]} onClick={() => onSquareClick(0)}/>
+                <SquareComponent className="b-bottom-right" state={gameState[1]} onClick={() => onSquareClick(1)}/>
+                <SquareComponent className="b-bottom" state={gameState[2]} onClick={() => onSquareClick(2)}/>
             </div>
             <div className="row jc-center">
-                <SquareComponent className="b-bottom-right" state={gameState[3]}/>
-                <SquareComponent className="b-bottom-right" state={gameState[4]}/>
-                <SquareComponent className="b-bottom" state={gameState[5]}/>
+                <SquareComponent className="b-bottom-right" state={gameState[3]} onClick={() => onSquareClick(3)}/>
+                <SquareComponent className="b-bottom-right" state={gameState[4]} onClick={() => onSquareClick(4)}/>
+                <SquareComponent className="b-bottom" state={gameState[5]} onClick={() => onSquareClick(5)}/>
             </div>
             <div className="row jc-center">
-                <SquareComponent className="b-right" state={gameState[6]}/>  
-                <SquareComponent className="b-right" state={gameState[7]}/>
-                <SquareComponent state={gameState[8]}/>
+                <SquareComponent className="b-right" state={gameState[6]} onClick={() => onSquareClick(6)}/>  
+                <SquareComponent className="b-right" state={gameState[7]} onClick={() => onSquareClick(7)}/>
+                <SquareComponent state={gameState[8]} onClick={() => onSquareClick(8)}/>
             </div>
             <button className="clear-button">Clear Game</button>
             <p>developed by Meruyert Karim</p>
--- src/SquareComponent.js
+++ src/SquareComponent.js
@@ -4,7 +4,7 @@ import React from 'react';
 function SquareComponent(props) {
     const classes = props.className ? `${props.className} square` : 'square';
     return (
-    <span className={classes}>
+    <span className={classes} onClick={props.onClick}>
         {props.state}
     </span>
     );
